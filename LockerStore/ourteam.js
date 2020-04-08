5f87457ddb5c23c6f3fc19dc3e945a822cb0563d
justinfat <62059196+justinfat@users.noreply.github.com>
2020-04-09 06:03:46 +0800

    Update ourteam.js

--- LockerStore/ourteam.js
+++ LockerStore/ourteam.js
@@ -2,10 +2,10 @@ $(function(){
 	$(document).ready(function(){
 		$(".pic_a").hover(function(){
 			$(".text_a").fadeIn();
-			$(".pic_a").attr('src','./public/images/about_us/our-team-undone/under_cover/undercover_wei_07.png');
+			$(".pic_a").attr("src","./public/images/about_us/our-team-undone/under_cover/undercover_wei_07.png");
 		},function(){
 			$(".text_a").fadeOut();
-			$(".pic_a").attr('src','./public/images/about_us/our-team-undone/no_cover/svg_file/nocover_wei.svg.png');
+			$(".pic_a").attr("src","./public/images/about_us/our-team-undone/no_cover/svg_file/nocover_wei.svg");
 		});
 		$(".pic_b").hover(function(){
 			$(".text_b").fadeIn();
