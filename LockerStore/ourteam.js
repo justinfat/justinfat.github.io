d169b760ea8b445bfe445db0c05861d82b3505d9
justinfat <j7768775@gmail.com>
2020-04-09 05:09:23 +0800

    ourteam.js

---
+++ LockerStore/ourteam.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".pic_a").mouseover(function(){
+		$(".text_a").fadeIn();
+	});
+	$(".pic_a").mouseout(function(){
+                $(".text_a").fadeOut;
+        }
+});
