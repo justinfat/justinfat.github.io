3551e68c8692d0e81e461e73f3c4143140ea51d8
JCP1014 <believe861014@gmail.com>
2020-04-13 10:39:10 +0800

    Put back contact_us and back_to_top

--- LockerStore/index.js
+++ LockerStore/index.js
@@ -1,3 +1,107 @@
 /*$('.btn').click(function(){
     $(this).toggleClass('clicked')
   })*/
+function on_a() {
+  document.getElementById("pic_a").className = "pic_a_in";
+  document.getElementById("cover_a").className = "cover_a_open";
+  document.getElementById("name_a").className = "name_a_in";
+  document.getElementById("intro_a").className = "intro_a_in";
+}
+function out_a() {
+  document.getElementById("pic_a").className = "pic_a_in";
+  document.getElementById("cover_a").className = "cover_a_close";
+  document.getElementById("name_a").className = "name_a_out";
+  document.getElementById("intro_a").className = "intro_a_out";
+}
+function on_b() {
+  document.getElementById("pic_b").className = "pic_a_in";
+  document.getElementById("cover_b").className = "cover_b_open";
+  document.getElementById("name_b").className = "name_b_in";
+  document.getElementById("intro_b").className = "intro_b_in";
+}
+function out_b() {
+  document.getElementById("pic_b").className = "pic_a_in";
+  document.getElementById("cover_b").className = "cover_b_close";
+  document.getElementById("name_b").className = "name_b_out";
+  document.getElementById("intro_b").className = "intro_b_out";
+}
+function on_c() {
+  document.getElementById("pic_c").className = "pic_a_in";
+  document.getElementById("cover_c").className = "cover_b_open";
+  document.getElementById("name_c").className = "name_b_in";
+  document.getElementById("intro_c").className = "intro_b_in";
+}
+function out_c() {
+  document.getElementById("pic_c").className = "pic_a_in";
+  document.getElementById("cover_c").className = "cover_b_close";
+  document.getElementById("name_c").className = "name_b_out";
+  document.getElementById("intro_c").className = "intro_b_out";
+}
+function on_d() {
+  document.getElementById("pic_d").className = "pic_a_in";
+  document.getElementById("cover_d").className = "cover_a_open";
+  document.getElementById("name_d").className = "name_a_in";
+  document.getElementById("intro_d").className = "intro_a_in";
+}
+function out_d() {
+  document.getElementById("pic_d").className = "pic_a_in";
+  document.getElementById("cover_d").className = "cover_a_close";
+  document.getElementById("name_d").className = "name_a_out";
+  document.getElementById("intro_d").className = "intro_a_out";
+}
+function on_e() {
+  document.getElementById("pic_e").className = "pic_a_in";
+  document.getElementById("cover_e").className = "cover_a_open";
+  document.getElementById("name_e").className = "name_a_in";
+  document.getElementById("intro_e").className = "intro_a_in";
+}
+function out_e() {
+  document.getElementById("pic_e").className = "pic_a_in";
+  document.getElementById("cover_e").className = "cover_a_close";
+  document.getElementById("name_e").className = "name_a_out";
+  document.getElementById("intro_e").className = "intro_a_out";
+ }
+function on_all() {
+  document.getElementById("pic_a").className = "pic_a_in";
+  document.getElementById("cover_a").className = "cover_a_open";
+  document.getElementById("name_a").className = "name_a_in";
+  document.getElementById("intro_a").className = "intro_a_in";
+  document.getElementById("pic_b").className = "pic_a_in";
+  document.getElementById("cover_b").className = "cover_b_open";
+  document.getElementById("name_b").className = "name_b_in";
+  document.getElementById("intro_b").className = "intro_b_in";
+  document.getElementById("pic_c").className = "pic_a_in";
+  document.getElementById("cover_c").className = "cover_b_open";
+  document.getElementById("name_c").className = "name_b_in";
+  document.getElementById("intro_c").className = "intro_b_in";
+  document.getElementById("pic_d").className = "pic_a_in";
+  document.getElementById("cover_d").className = "cover_a_open";
+  document.getElementById("name_d").className = "name_a_in";
+  document.getElementById("intro_d").className = "intro_a_in";
+  document.getElementById("pic_e").className = "pic_a_in";
+  document.getElementById("cover_e").className = "cover_a_open";
+  document.getElementById("name_e").className = "name_a_in";
+  document.getElementById("intro_e").className = "intro_a_in";
+}
+function out_all() {
+  document.getElementById("pic_a").className = "pic_a_in";
+  document.getElementById("cover_a").className = "cover_a_close";
+  document.getElementById("name_a").className = "name_a_out";
+  document.getElementById("intro_a").className = "intro_a_out";
+  document.getElementById("pic_b").className = "pic_a_in";
+  document.getElementById("cover_b").className = "cover_b_close";
+  document.getElementById("name_b").className = "name_b_out";
+  document.getElementById("intro_b").className = "intro_b_out";
+  document.getElementById("pic_c").className = "pic_a_in";
+  document.getElementById("cover_c").className = "cover_b_close";
+  document.getElementById("name_c").className = "name_b_out";
+  document.getElementById("intro_c").className = "intro_b_out";
+  document.getElementById("pic_d").className = "pic_a_in";
+  document.getElementById("cover_d").className = "cover_a_close";
+  document.getElementById("name_d").className = "name_a_out";
+  document.getElementById("intro_d").className = "intro_a_out";
+  document.getElementById("pic_e").className = "pic_a_in";
+  document.getElementById("cover_e").className = "cover_a_close";
+  document.getElementById("name_e").className = "name_a_out";
+  document.getElementById("intro_e").className = "intro_a_out";
+}
