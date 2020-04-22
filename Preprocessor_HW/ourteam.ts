78f029bfe1dd6e06712600c1928d72e443bcd389
justinfat <j7768775@gmail.com>
2020-04-23 03:38:54 +0800

    #

--- Preprocessor_HW/ourteam.ts
+++ Preprocessor_HW/ourteam.ts
@@ -1,1 +1,46 @@
-function 
+$(document).ready(() => {
+	$(".coverpic_a").hover(() => {
+		$(".text_a").fadeIn();
+		$(".pic_a").fadeIn();
+	},()=>{});
+	$(".pic_a").hover(() => {
+		$(".text_a").fadeOut();
+		$(".pic_a").fadeOut();
+	},()=>{});
+
+	$(".coverpic_b").hover(() => {
+                $(".text_b").fadeIn();
+                $(".pic_b").fadeIn();
+        },()=>{});
+        $(".pic_b").hover(() => {
+                $(".text_b").fadeOut();
+                $(".pic_b").fadeOut();
+        },()=>{});
+
+	$(".coverpic_c").hover(() => {
+                $(".text_c").fadeIn();
+                $(".pic_c").fadeIn();
+        },()=>{});
+        $(".pic_c").hover(() => {
+                $(".text_c").fadeOut();
+                $(".pic_c").fadeOut();
+        },()=>{});
+
+        $(".coverpic_d").hover(() => {
+                $(".text_d").fadeIn();
+                $(".pic_d").fadeIn();
+        },()=>{});
+        $(".pic_d").hover(() => {
+                $(".text_d").fadeOut();
+                $(".pic_d").fadeOut();
+        },()=>{});
+
+	$(".coverpic_e").hover(() => {
+                $(".text_e").fadeIn();
+                $(".pic_e").fadeIn();
+        },()=>{});
+        $(".pic_e").hover(() => {
+                $(".text_e").fadeOut();
+                $(".pic_e").fadeOut();
+        },()=>{});	
+})
