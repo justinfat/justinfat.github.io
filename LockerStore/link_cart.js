6a756aed9cfc86d79fef88d59c933162d4f7886f
hkps87 <61841733+hkps87@users.noreply.github.com>
2020-05-17 00:00:35 +0800

    Create link_cart.js

---
+++ LockerStore/link_cart.js
@@ -0,0 +1,7 @@
+function openwin() {
+   
+    window.open ("https://justinfat.github.io/LockerStore/shopping_cart.html", "newwindow", "height=100, width=400, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, status=no")
+   
+    //寫成一行
+   
+    }
