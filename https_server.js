e5c125c9b6963db979555ca0ea2d4f1fde91d101
JCP1014 <believe861014@gmail.com>
2020-05-20 19:34:26 +0800

    Update https_server

--- https_server.js
+++ https_server.js
@@ -39,23 +39,77 @@ app.get('./sign_up', (req, res) => {
   res.send(`${req.query.account}`)
 })
 
-app.get('/insertInfo', (req, res) => {
+app.get('/insertLocker', (req, res) => {
   connection.query(
     `INSERT INTO lockers (id, lat, lng, name, addr) 
     SELECT NULL, '${req.query.lat}', '${req.query.lng}', '${req.query.name}', '${req.query.addr}' FROM DUAL
     WHERE NOT EXISTS (SELECT 1 FROM lockers WHERE name = '${req.query.name}' LIMIT 1);`, (err, result) => {
-      if (err) console.log('fail to insert:', err)
-    })
+    if (err) console.log('fail to insert:', err)
+  })
   res.send(`ok`)
 })
 
-app.get('/searchInfo', (req, res) => {
+app.get('/searchLocker', (req, res) => {
   connection.query(
     `SELECT name, addr FROM lockers WHERE lat = '${req.query.lat}' AND lng = '${req.query.lng}'`, function (error, results, fields) {
       if (error) throw error
       res.send({
         name: results[0].name,
         addr: results[0].addr,
+      })
+    })
+})
+
+app.get('/searchTag', (req, res) => {
+  var arr = req.query.keywords.split('#');
+  var keywords = arr.join('');
+  connection.query(
+    `SELECT * FROM shops WHERE MATCH (name, tag) AGAINST ('${keywords}' IN BOOLEAN MODE)`, function (error, results, fields) {
+      if (error) throw error
+      res.send({
+        results: results,
+      })
+    })
+})
+
+app.get('/searchShop', (req, res) => {
+  connection.query(
+    `SELECT * FROM shops WHERE name = '${req.query.name}'`, function (error, results, fields) {
+      if (error) throw error
+      // if (error) {
+      //   res.send('error')
+      // }
+      else {
+          res.send({
+            id: results[0].id,
+            name: results[0].name,
+            starNum: results[0].starNum,
+            commentNum: results[0].commentNum,
+            tag: results[0].tag,
+            addr: results[0].addr,
+            tel: results[0].tel,
+            img: results[0].img,
+          })
+        }
+    })
+})
+
+app.get('/getItem_single', (req, res) => {
+  connection.query(
+    `SELECT * FROM shop_${req.query.id} WHERE isGroup = 0 ORDER BY img DESC`, function (error, results, fields) {
+      // if (error) throw error
+      res.send({
+        results: results,
+      })
+    })
+})
+
+app.get('/getItem_group', (req, res) => {
+  connection.query(
+    `SELECT * FROM shop_${req.query.id} WHERE isGroup = 1 ORDER BY img DESC`, function (error, results, fields) {
+      // if (error) throw error
+      res.send({
+        results: results,
       })
     })
 })
