4fed61623b3503ac06f5e7d85808557d406d3231
hkps87 <61841733+hkps87@users.noreply.github.com>
2020-06-30 15:28:28 +0800

    Update order_list.js

--- LockerStore/order_list.js
+++ LockerStore/order_list.js
@@ -15,3 +15,23 @@ function showDetail()
       $('.left_side').css("display", "inline-block");
      }
  }
+function showLeft()
+{
+    $('.order-content-left').css("display", "block");
+    $('.order-content-mid').css("display", "none");
+    $('.order-content-right').css("display", "none");
+}
+
+function showMid()
+{
+    $('.order-content-left').css("display", "none");
+    $('.order-content-mid').css("display", "block");
+    $('.order-content-right').css("display", "none");
+}
+
+function showRight()
+{
+    $('.order-content-left').css("display", "none");
+    $('.order-content-mid').css("display", "none");
+    $('.order-content-right').css("display", "block");
+}
