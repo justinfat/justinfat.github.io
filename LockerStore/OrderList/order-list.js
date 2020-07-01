84bdc6ab719219b11273542ac10838d2fcdb3dbd
justinfat <j7768775@gmail.com>
2020-07-01 23:06:00 +0800

    07012305

--- LockerStore/OrderList/order-list.js
+++ LockerStore/OrderList/order-list.js
@@ -1,80 +1,75 @@
-$(function() {
-    // This button will increment the value
-    $('.spc-qty-plus').click(function(e) {
-      // Stop acting like a button
-      e.preventDefault();
-      // Get the field name
-      fieldName = $(this).attr('field');
-      // Get its current value
-      var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-      // If is not undefined
-      if (!isNaN(currentVal)) {
-        // Increment
-        $('input[name=' + fieldName + ']').val(currentVal + 1);
-      } else {
-        // Otherwise put a 0 there
-        $('input[name=' + fieldName + ']').val(0);
-      }
-    });
-    // This button will decrement the value till 0
-    $(".spc-qty-minus").click(function(e) {
-      // Stop acting like a button
-      e.preventDefault();
-      // Get the field name
-      fieldName = $(this).attr('field');
-      // Get its current value
-      var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-      // If it isn't undefined or its greater than 0
-      if (!isNaN(currentVal) && currentVal > 0) {
-        // Decrement one
-        $('input[name=' + fieldName + ']').val(currentVal - 1);
-      } else {
-        // Otherwise put a 0 there
-        $('input[name=' + fieldName + ']').val(0);
-     }
-   });
-});
+var a = document.getElementById('ol-grouping-button');
+var b = document.getElementById('ol-undelivered-button');
+var c = document.getElementById('ol-arrived-button');
+var d = document.getElementById('ol-done-button');
 
-function SelectAll(obj,cName) {
-    var checkboxs = document.getElementsByName(cName);
-    for(var i=0;i<checkboxs.length;i++){checkboxs[i].checked = obj.checked;}
-};
-
-/*$(function () {
-    var checkboxs = document.getElementsByClassName("spc-checkbox");
-    var orderButton = document.getElementById("spc-order");
-    
-    for(var i=0;i<checkboxs.length;i++){
-        var a = 0;
-
-        if(checkboxs[i].checked == true) { a++; }
-
-        if(a != 0){
-            orderButton.disabled = false;
-        }else{
-            orderButton.disabled = true;
-        }
-    }
-     
-});*/
-
-function SwitchToCheckout(){
-    $('.screen').css("display","none");
-    $('#check-out').css("display","block");
+function SwitchToGrouping(){
+  $('.ol-page').css("display","none");
+  $('#ol-grouping').css("display","block");
+  a.style.color = 'white';
+  a.style.backgroundColor = '#2C50A1';
+  b.style.color = '#2C50A1';
+  b.style.backgroundColor = 'white';
+  c.style.color = '#2C50A1';
+  c.style.backgroundColor = 'white';
+  d.style.color = '#2C50A1';
+  d.style.backgroundColor = 'white';
 }
-function SwitchToShoppingcart(){
-    $('.screen').css("display","none");
-    $('#shopping-cart').css("display","block");
+function SwitchToUndelivered(){
+  $('.ol-page').css("display","none");
+  $('#ol-undelivered').css("display","block");
+  b.style.color = 'white';
+  b.style.backgroundColor = '#2C50A1';
+  a.style.color = '#2C50A1';
+  a.style.backgroundColor = 'white';
+  c.style.color = '#2C50A1';
+  c.style.backgroundColor = 'white';
+  d.style.color = '#2C50A1';
+  d.style.backgroundColor = 'white';
 }
-function SpcEdit(Obj){
-    Obj.innerText = "完成";
-    Obj.setAttribute("onclick","SpcEditDone(this)");
-    $('.spc-qty').css("display","none");
-    $('.spc-item-delete').css("display","block");
+function SwitchToArrived(){
+  $('.ol-page').css("display","none");
+  $('#ol-arrived').css("display","block");
+  c.style.color = 'white';
+  c.style.backgroundColor = '#2C50A1';
+  a.style.color = '#2C50A1';
+  a.style.backgroundColor = 'white';
+  b.style.color = '#2C50A1';
+  b.style.backgroundColor = 'white';
+  d.style.color = '#2C50A1';
+  d.style.backgroundColor = 'white';
 }
-function SpcEditDone(Obj){
-    Obj.innerText = "編輯";
-    Obj.setAttribute("onclick","SpcEdit(this)");
-    $('.spc-qty').css("display","block");
-    $('.spc-item-delete').css("display","none");
+function SwitchToDone(){
+  $('.ol-page').css("display","none");
+  $('#ol-done').css("display","block");
+  d.style.color = 'white';
+  d.style.backgroundColor = '#2C50A1';
+  a.style.color = '#2C50A1';
+  a.style.backgroundColor = 'white';
+  b.style.color = '#2C50A1';
+  b.style.backgroundColor = 'white';
+  c.style.color = '#2C50A1';
+  c.style.backgroundColor = 'white';
+}
+
+function SeeDetail(Obj){
+  Obj.setAttribute("onclick","UnSeeDetail(this)");
+  Obj.setAttribute("style", "transform:rotate(180deg)");
+  $('.ol-item-list').css("display","block");
+}
+function UnSeeDetail(Obj){
+  Obj.setAttribute("onclick","SeeDetail(this)");
+  Obj.setAttribute("style", "transform:rotate(0)");
+  $('.ol-item-list').css("display","none");
+}
+
+function SeeDetailPickup(Obj){
+  Obj.setAttribute("onclick","UnSeeDetailPickup(this)");
+  Obj.setAttribute("style", "transform:rotate(180deg)");
+  $('.ol-pickup-info').css("display","block");
+}
+function UnSeeDetailPickup(Obj){
+  Obj.setAttribute("onclick","SeeDetailPickup(this)");
+  Obj.setAttribute("style", "transform:rotate(0)");
+  $('.ol-pickup-info').css("display","none");
 }
