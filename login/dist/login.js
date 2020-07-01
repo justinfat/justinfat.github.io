d47187aa328438e1955cd93b83e0cf755f88bb16
justinfat <justinfat@mbilabta@gmail.com>
2020-07-01 13:34:26 +0800

    login

---
+++ login/dist/login.js
@@ -0,0 +1,31 @@
+function SwitchToVerify(){
+  $('.screen').css("display","none");
+  $('#page-verify').css("display","block");
+}
+function SwitchToCreditcard(){
+  $('.screen').css("display","none");
+  $('#page-credit-card').css("display","block");
+}
+function SwitchToSigninAccount(){
+  $('.screen').css("display","none");
+  $('#page-sign-in-account').css("display","block");
+}
+function SwitchToSignup(){
+  $('.screen').css("display","none");
+  $('#page-sign-up').css("display","block");
+}
+function SwitchToLocate(){
+  $('.screen').css("display","none");
+  $('#page-locate').css("display","block");
+}
+function SwitchToSigninPassword(){
+  $('.screen').css("display","none");
+  $('#page-sign-in-password').css("display","block");
+}
+function SwitchToSigninVerify(){
+  $('.screen').css("display","none");
+  $('#page-sign-in-verify').css("display","block");
+}
+
+
+
